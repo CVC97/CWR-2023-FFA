60950b8c9dc0d2213587658bdd623cdcb7caa44c
carlo.voncarnap <carlo.voncarnap@stud.uni-goettingen.de>
2023-05-13 18:11:13 +0200

    A14 Update von Abgabe.

--- A14_Schwingungssensor.c
+++ A14_Schwingungssensor.c
@@ -5,14 +5,14 @@
 
 
 // Physikalische Konstanten des Systems
-const int N = 2;                                        // Dimension des Problems N
-const double mass = 1;                                  // Masse mass
-const double k = 10;                                    // Federkonstante k beider Federn           
-const double r_0 = 1;                                   // Ruhelänge r_0 beider Federn
-const double r_A[2] = {-1, 0};                          // Position der Verankerung der linken Feder r_A
-const double r_B[2] = {1, 0};                           // Position der Verankerung der rechten Feder r_b
+const int N = 2;                                                                // Dimension N des Problems
+const double mass = 1;                                                          // Masse mass
+const double k = 10;                                                            // Federkonstante k beider Federn           
+const double r_0 = 1;                                                           // Ruhelänge r_0 beider Federn
+const double r_A[2] = {-1, 0};                                                  // Position der Verankerung der linken Feder r_A
+const double r_B[2] = {1, 0};                                                   // Position der Verankerung der rechten Feder r_b
 
-const double smoothing_factor = 1e-6;                   // Vermeidung von numerischen Fehlern bei Division (durch 0)
+const double smoothing_factor = 1e-6;                                           // Vermeidung von numerischen Fehlern bei Division (durch 0)
 
 
 // (gewöhnliche) Differentialgleichung des Federsystems
@@ -45,30 +45,30 @@ int ODE_dual_springs(double t, const double y[], double f[], void *params) {
 
 int main(void) {
     // Simulationsparameter
-    int dimension = 2 * N;
-    double T_x = 2 * CVC_PI / sqrt(2*k);
-    double x_0 = -0.5;
-    double t = 0, delta_t = 1e-4;
+    int dimension = 2 * N;                                                      // Dimensionalität des Zustandsvektors des Systems
+    double T_x = 2 * CVC_PI / sqrt(2*k);                                        // analytische Periodendauer T_x
+    double x_0 = -0.5;                                                          // Startkoordinate x_0
+    double t = 0, delta_t = 1e-4;                                               // Startzeit t = 0, Größe der Zeitschritte delta_t
 
 
 // +++++ (Aufgabe 4, 5: Position x(t) in Abhängigkeit der Zeit) +++++
 
     // Initialisierung der Startposition der Masse
-    double y_euler[4] = {-0.5, 0}; 
-    double y_rk4[4] = {-0.5, 0};
+    double y_euler[4] = {x_0, 0}; 
+    double y_rk4[4] = {x_0, 0};
 
     // numerische Nullstellensuche über Vorzeichenwechsel der x-Koordinate
     int euler_number_zero_crossings = 0;                                        // Anzahl der Nulldurchgänge (Euler)
     int rk4_number_zero_crossings = 0;                                          // Anzahl der Nulldurchgänge (RK4)
-    double *euler_zero_crossings = (double*) malloc(sizeof(double));            // Array mit Nulldurchgängen (Euler)
-    double *rk4_zero_crossings = (double*) malloc(sizeof(double));              // Array mit Nulldurchgängen (RK4)
+    double *euler_zero_crossings = (double*) malloc(sizeof(double));            // Reservierung Speicher für Nulldurchgänge (Euler)
+    double *rk4_zero_crossings = (double*) malloc(sizeof(double));              // Reservierung Speicher für Nulldurchgänge (RK4)
 
     // Ausgabedatei
     FILE* pos_file = fopen("A14_Schwingungssensor_pos.csv", "w"); 
     fprintf(pos_file, "Zeit, x(t) [Euler], y(t) [Euler], x(t) [RK4], y(t) [RK4]\n");
     fprintf(pos_file, "%g, %g, %g, %g, %g\n", t, y_euler[0], y_euler[1], y_rk4[0], y_rk4[1]);
 
-     // Integration bis 10 * T_x
+    // Integration über 10 Periodendauern T_x
     while (t <= 10 * T_x) {
         t += delta_t;
 
@@ -81,38 +81,37 @@ int main(void) {
         double x_post_euler = y_euler[0];                                       // x-Koordinate nach Integration (Euler) 
         double x_post_rk4 = y_rk4[0];                                           // x-Koordinate nach Integration (Euler) 
 
-        // Zeiten der Nulldurchgänge in das jeweilige Array (Euler, RK4) nach Anpassen der Arraygröße mit realloc
+        // Bestimmung der Zeiten der Nulldurchgänge
         if (x_pre_euler * x_post_euler < 0) {                                   // Prüfen eines Nulldurchganges über Vorzeichenwechsel (Euler)         
             euler_number_zero_crossings++;               
             euler_zero_crossings = (double*) realloc(euler_zero_crossings, sizeof(double) * euler_number_zero_crossings);
-            euler_zero_crossings[euler_number_zero_crossings - 1] = t;              
+            euler_zero_crossings[euler_number_zero_crossings - 1] = t;          // Eintragen des Nulldurchganges in vergrößerten Speicherbereich (Euler)             
 
         }
         if (x_pre_rk4 * x_post_rk4 < 0) {                                       // Prüfen eines Nulldurchganges über Vorzeichenwechsel (RK4)
             rk4_number_zero_crossings++;
             rk4_zero_crossings = (double*) realloc(rk4_zero_crossings, sizeof(double) * rk4_number_zero_crossings);
-            rk4_zero_crossings[rk4_number_zero_crossings - 1] = t;
+            rk4_zero_crossings[rk4_number_zero_crossings - 1] = t;              // // Eintragen des Nulldurchganges in vergrößerten Speicherbereich (RK4) 
 
         }
         fprintf(pos_file, "%g, %g, %g, %g, %g\n", t, y_euler[0], y_euler[1], y_rk4[0], y_rk4[1]);
     }
 
-    // Ausgabe der Periodendauer als 2 * [t(Nulldurchgang N) - t(Nulldurchgang 1)] / N für Nulldurchgänge > 1
+    // Ausgabe der Periodendauer (wenn möglich) 
     printf("Periodendauer T_x (analytisch): %g\n", T_x);
     if (euler_number_zero_crossings < 2) {                                      // Prüfen ob ausreichend Nulldurchgänge (Euler)
         printf("ERROR! Insufficient number of zero-crossings for Euler.");
     } else {                                                                    // Berechnung T_x für ausreichend Nulldurchgänge (Euler)
         double T_x_Euler = 2.0 / euler_number_zero_crossings * (euler_zero_crossings[euler_number_zero_crossings - 1] - euler_zero_crossings[0]);
-        printf("Periodendauer T_x (Euler): %g\n", T_x_Euler);                  
+        printf("Periodendauer T_x (Euler): %g\n", T_x_Euler);                   // Printen Periodendauer als 2 * [t(Nulldurchgang N) - t(Nulldurchgang 1)] / N für Nulldurchgänge > 1 (Euler)
     }
     if (rk4_number_zero_crossings < 2) {                                        // Prüfen ob ausreichend Nulldurchgänge (RK4)
         printf("ERROR! Insufficient number of zero-crossings for RK4.");
     } else {                                                                    // Berechnung T_x für ausreichend Nulldurchgänge (RK4)                           
         double T_x_RK4 = 2.0 / rk4_number_zero_crossings * (rk4_zero_crossings[rk4_number_zero_crossings - 1] - rk4_zero_crossings[0]);
-        printf("Periodendauer T_x (RK4): %g\n", T_x_RK4);                     
+        printf("Periodendauer T_x (RK4): %g\n", T_x_RK4);                       // Printen Periodendauer als 2 * [t(Nulldurchgang N) - t(Nulldurchgang 1)] / N für Nulldurchgänge > 1 (RK4)  
     }
-    fclose(pos_file);
-    free(euler_zero_crossings), free(rk4_zero_crossings);
+    fclose(pos_file), free(euler_zero_crossings), free(rk4_zero_crossings);
 
 
 // +++++ (Aufgabe 6: Abweichung der numerischen von der analytischen Lösung für variierte Zeitschritte delta_t) +++++
@@ -121,22 +120,22 @@ int main(void) {
     FILE* res_file = fopen("A14_Schwingungssensor_res.csv", "w");
     fprintf(res_file, "delta_t, Residue Euler, Residue RK4\n"); 
 
-    // Itereation über 100 logarithmisch gleichverteilte delta_t
+    // Itereation über 100 logarithmisch gleichverteilte Zeitschritte delta_t
     for (int i = 0; i < 100; i++) {
-        t = 0, delta_t = pow(10, -1-4*i/99.0);          // Neuberechnung der Zeitparameter für jede Integration
+        t = 0, delta_t = pow(10, -1-4*i/99.0);                                  // Neuberechnung der Zeitparameter für jede Integration
 
         // (Re)initialisierung der Startposition der Masse
-        double y_euler_res[4] = {-0.5, 0}; 
-        double y_rk4_res[4] = {-0.5, 0};
+        double y_euler_res[4] = {x_0, 0}; 
+        double y_rk4_res[4] = {x_0, 0};
 
-        // Integration bis 10 * T_x
+        // Integration über 10 Periodendauern T_x
         while (t + delta_t < 10 * T_x) {
             t += delta_t;
             euler_step(t, delta_t, y_euler_res, ODE_dual_springs, dimension, NULL);
             rk4_step(t, delta_t, y_rk4_res, ODE_dual_springs, dimension, NULL);
         }
         
-        // Verwendung eines zusätzlichen Zeitschritts (Differenz zur analytischen T_x) für Vergleichbarkeit mit analytischer Lösung
+        // zusätzlicher Zeitschritt (Differenz zur analytischen T_x) für Vergleichbarkeit mit analytischer Lösung
         euler_step(t, 10*T_x - t, y_euler_res, ODE_dual_springs, dimension, NULL);
         rk4_step(t, 10*T_x - t, y_rk4_res, ODE_dual_springs, dimension, NULL);
 
@@ -159,7 +158,7 @@ int main(void) {
     fprintf(fancy_pos_file, "Zeit, x1(t), y1(t), x2(t), y2(t), x3(t), y3(t)\n");
     fprintf(fancy_pos_file, "%g, %g, %g, %g, %g, %g, %g\n", t, y1_rk4[0], y1_rk4[1], y2_rk4[0], y2_rk4[1], y3_rk4[0], y3_rk4[1]);
 
-     // Integration bis 10 * T_x
+    // Integration über 10 Periodendauern T_x
     while (t <= 10 * T_x) {
         t += delta_t;
         rk4_step(t, delta_t, y1_rk4, ODE_dual_springs, dimension, NULL);
