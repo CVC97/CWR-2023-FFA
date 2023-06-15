4107f13b642dbd60e99d3ee0984f791e10878acd
carlo.voncarnap <carlo.voncarnap@stud.uni-goettingen.de>
2023-06-15 19:03:50 +0200

    Zerstörung Welt mit Tsunami.

--- A26_Wellenphaenomene.c
+++ A26_Wellenphaenomene.c
@@ -24,9 +24,9 @@ const double OCEAN_X_RIFF_RIGHT = -300e3;                       // rechter Rand 
 
 // Integrationsparameter
 const double T_MAX = 22000;
-const double DELTA_X = 1e4;
-const double DELTA_T = 10;
-const int N = (OCEAN_X_RIGHT - OCEAN_X_LEFT) / DELTA_X + 1;
+const double DELTA_X = 1000;
+const double DELTA_T = 1;
+const int N = (OCEAN_X_RIGHT - OCEAN_X_LEFT) / DELTA_X;
 
 
 // Struktur der Parameter des d'Alembert-Integrators
@@ -45,7 +45,7 @@ double height_ocean(double x) {
     } else if (OCEAN_X_BEACH_LEFT <= x && x < OCEAN_X_BEACH_RIGHT) {
         return OCEAN_HEIGHT_RIFF;
     } else if (OCEAN_X_BEACH_RIGHT <= x && x < OCEAN_X_RIFF_RIGHT) {
-        return OCEAN_HEIGHT_RIFF + (OCEAN_HEIGHT_RIFF-OCEAN_HEIGHT) * cvc_npow(sin(cvc_PI/2 * (OCEAN_X_BEACH_RIGHT-x) / (OCEAN_X_BEACH_RIGHT-OCEAN_X_RIFF_RIGHT)), 2);
+        return OCEAN_HEIGHT_RIFF - (OCEAN_HEIGHT_RIFF-OCEAN_HEIGHT) * cvc_npow(sin(cvc_PI/2 * (OCEAN_X_BEACH_RIGHT-x) / (OCEAN_X_BEACH_RIGHT-OCEAN_X_RIFF_RIGHT)), 2);
     } else if (OCEAN_X_RIFF_RIGHT <= x && x < OCEAN_X_RIGHT) {
         return OCEAN_HEIGHT;
     } else {
@@ -62,58 +62,66 @@ double wave_excitation_A(double t) {
 
 // Wellengeschwindigkeit für ein gegebenes x
 double wave_speed_u(double x) {
-    // return sqrt(cvc_EARTH_GRAVITATION * height_ocean(x));
-    return 1;
+    return sqrt(cvc_EARTH_GRAVITATION * height_ocean(x));
 }
 
 
 // Berechnet den Zustand der Welle des nächsten Zeitschrittees
-int dAlembertFTCS(double t, double y[], struct params_dAlembert) {
-    double delta_x = params_dAlembert.delta_x;
-    double delta_t = params_dAlembert.delta_t;
-
-    // Berechnung der Wellengeschwindigkeit für x, x_(i-1) und x_(i+1)
-    double u_i = wave_speed_u(x);
-    double u_i_minus = wave_speed_u(x - delta_x);
-    double u_i_plus = wave_speed_u(x + delta_x);
-
-    // Berechnung des Parameters beta^2 aus den Wellengeschwindigkeiten für x, x_(i-1) und x_(i+1)
-    double beta_i_squared = cvc_npow(u_i * delta_t / delta_x, 2);
-    double beta_i_minus_squared = cvc_npow(u_i_minus * delta_t / delta_x, 2);
-    double beta_i_plus_squared = cvc_npow(u_i_plus * delta_t / delta_x, 2);
+int dAlembertFTCS(double t, double y_old[], double y[], struct params_dAlembert params) {
+    double delta_x = params.delta_x;
+    double delta_t = params.delta_t;
 
     // Berechnung des Zustandes für den nächsten Zeitschritt
     double *y_temp = (double*) malloc(N * sizeof(double));
     for(size_t i = 0; i < N; i++) {
         double x = OCEAN_X_LEFT + (i * delta_x);
 
+        // Werte des vorherigen Zeitschrittes für die Berechnung des Nächsten
         double f = y[i % N];
         double f_west = y[(i - 1) % N];
         double f_east = y[(i + 1) % N];
+        double f_t_minus = y_old[i % N];
 
-        // if (0 <= t && t <= 2*cvc_PI/OSZ_OMEGA) {
-        //     double f_west_ghost = A(t);                                                     // linker Ghost
-        // } else {
-        //     double f_west_ghost = 0;
-        // }
+        // Berechnung der Wellengeschwindigkeit für x, x_(i-1) und x_(i+1)
+        double u_i = wave_speed_u(x);
+        double u_i_minus = wave_speed_u(x - delta_x);
+        double u_i_plus = wave_speed_u(x + delta_x);
 
-        double f_west_ghost = A(t);                                                         // linker Ghost
-        double f_east_ghost = 0;                                                            // rechter Ghost
-        double f_t_minus = f + 1.0/2 * beta_i_squared * (f_west - 2*f + f_east);            // Berechnung f_(t-1) [sehr dubios!!!!!]
+        // Berechnung des Parameters beta^2 aus den Wellengeschwindigkeiten für x, x_(i-1) und x_(i+1)
+        double beta_i_squared = cvc_npow(u_i * delta_t / delta_x, 2);
+        double beta_i_minus_squared = cvc_npow(u_i_minus * delta_t / delta_x, 2);
+        double beta_i_plus_squared = cvc_npow(u_i_plus * delta_t / delta_x, 2);
 
-        double delta_f_t = cvc_diff
+        if (beta_i_squared > 1) {
+            printf("DICK\n");
+        }
+
+        double f_east_ghost = 0;                                                                    // rechter Ghost
+        double f_west_ghost;
+
+        if (0 <= t && t <= 2*cvc_PI/OSZ_OMEGA) {
+            f_west_ghost = wave_excitation_A(t);                                                 // linker Ghost
+        } else {
+            f_west_ghost = 0;
+        }
+
+        // double f_west_ghost = wave_excitation_A(t);                                                 // linker Ghost
+        
+
+        double delta_f_t = 0; //cvc_diff
 
         if (i == 0) {
-            y_temp[i] = 2*(1-beta_squared)*f - f_t_minus + beta_i_squared*(f_east_ghost+f_west) + 1.0/4*(beta_i_plus_squared-beta_i_minus_squared)*(f_east_ghost-f_west);
+            y_temp[i] = 2*(1-beta_i_squared)*f - f_t_minus + beta_i_squared*(f_east+f_west_ghost) + 1.0/4*(beta_i_plus_squared-beta_i_minus_squared)*(f_east-f_west_ghost);
         } else if (i == N - 1) {
-            y_temp[i] = 2*(1-beta_squared)*f - f_t_minus + beta_i_squared*(f_east+f_west_ghost) + 1.0/4*(beta_i_plus_squared-beta_i_minus_squared)*(f_east-f_west_ghost);
+            y_temp[i] = 2*(1-beta_i_squared)*f - f_t_minus + beta_i_squared*(f_east_ghost+f_west) + 1.0/4*(beta_i_plus_squared-beta_i_minus_squared)*(f_east_ghost-f_west);
         } else {
-            y_temp[i] = 2*(1-beta_squared)*f - f_t_minus + beta_i_squared*(f_east+f_west) + 1.0/4*(beta_i_plus_squared-beta_i_minus_squared)*(f_east-f_west);
+            y_temp[i] = 2*(1-beta_i_squared)*f - f_t_minus + beta_i_squared*(f_east+f_west) + 1.0/4*(beta_i_plus_squared-beta_i_minus_squared)*(f_east-f_west);
         }
     }
 
     // Übertragen der neuen Werte in das Ursprungsaray
     for(int i = 0; i < N; i++) {
+        y_old[i] = y[i];
         y[i] = y_temp[i];
     }
 
@@ -126,26 +134,34 @@ int main(void) {
 
     // Initialisieren des Ozeans
     double *y_wave = (double*) calloc(N, sizeof(double));
+    double *y_wave_old = (double*) calloc(N, sizeof(double));
     double x = OCEAN_X_LEFT;                                                                // Laufvariable der Position im Ozean
     double t = 0;                                            
 
     FILE* ocean_file = fopen("data/A26_Tsunami_data.csv", "w");
-    fprintf(ocean_file, "%g", x);
-    for (int n_x = 0; n_x < N) {
-        fprintf(ocean_file, ", %g", (n_x+1) * DELTA_X);
+    fprintf(ocean_file, "0");
+    for (int n_x = 0; n_x < N; n_x++) {
+        fprintf(ocean_file, ", %g", n_x * DELTA_X);
     }
 
     // Übergabestruktur von den d'Alembert-Integrator
-    struct params_dAlembert = params_struct;
+    struct params_dAlembert params_struct;
 
     while (t < T_MAX) {
         params_struct.delta_t = DELTA_T;
         params_struct.delta_x = DELTA_X;
 
-        dAlembertFTCS(t, y_wave, params_struct);  
+        dAlembertFTCS(t, y_wave_old, y_wave, params_struct);
 
+        fprintf(ocean_file, "\n%g", t);
+        for (int n_x = 0; n_x < N; n_x++) {
+            fprintf(ocean_file, ", %g", y_wave[n_x]);
+        }
 
         t += DELTA_T;
     }
+    fclose(ocean_file);
+    free(y_wave);   
+    free(y_wave_old);
     return 0;
 }
